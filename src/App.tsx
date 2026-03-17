77cec29106960cb61beb2539fd91308681c6a3b2
Marilia da Silva Feitosa <marilia.feitosa.contato@gmail.com>
2026-03-17 11:43:36 -0300

    :sparkles: feat: adição do componente Footer e atualização do layout no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import Navbar from "./components/shared/navbar"
+import Footer from '@/components/shared/footer'
+import Navbar from '@/components/shared/navbar'
+// import HomePage from '@/pages/HomePage'
+// import LoginPage from './pages/LoginPage'
+
 function App() {
   return (
     <>
-      <div className="flex flex-col min-h-svh">
+      <div className='flex flex-col min-h-svh'>
         <Navbar />
-        <main className="flex-1"> Contéudo principal</main>
-        <footer>Rodapé</footer>
+        <main className='flex-1 flex flex-col'>
+          {/* <HomePage /> */}
+          {/* <LoginPage /> */}
+        </main>
+        <Footer />
       </div>
     </>
   )
