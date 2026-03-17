35b1401bba48a9824a40065586c9755b85dc8cb5
Marilia da Silva Feitosa <marilia.feitosa.contato@gmail.com>
2026-03-17 17:00:32 -0300

    :sparkles: feat: adição da página Home e dos componentes HeroSectione CtaSection e atualização do App para incluir o HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Footer from '@/components/shared/footer'
 import Navbar from '@/components/shared/navbar'
-// import HomePage from '@/pages/HomePage'
-// import LoginPage from './pages/LoginPage'
+import HomePage from '@/pages/HomePage'
+//import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <div className='flex flex-col min-h-svh'>
         <Navbar />
         <main className='flex-1 flex flex-col'>
-          {/* <HomePage /> */}
+          <HomePage />
           {/* <LoginPage /> */}
         </main>
         <Footer />
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import CtaSection from "@/components/homepage/cta-section"
+//import FaqSection from "@/components/homepage/faq-section"
+//import FeatureSection from "@/components/homepage/feature-section"
+import HeroSection from "@/components/homepage/hero-section"
+
+function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      {/* <FeatureSection />
+      //<FaqSection />*/}
+      <CtaSection />
+    </>
+  )
+}
+
+export default HomePage
