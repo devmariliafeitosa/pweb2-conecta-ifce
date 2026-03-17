e31d0e20a3c09f9a68b28c2ba22e54a6d9293c5b
Marilia da Silva Feitosa <marilia.feitosa.contato@gmail.com>
2026-03-16 22:29:05 -0300

    style: Adição de Alguns arquivos em branco no projeto e criação de novas pastas

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,120 @@
+/*
+import Brand from "@/components/shared/brand"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { EyeIcon, EyeOffIcon } from "lucide-react"
+import { useState } from "react"
+
+function LoginPage() {
+
+  const [showPass, setShowPass] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassowrd] = useState<string>("")
+  const [isLoading, setLoading] = useState<boolean>(false)
+
+  const HandleSubmit = async (event: React.SubmitEvent) =>{
+    event.preventDefault()
+
+
+    const response = await fetch(
+      "https://conectaifce-api.proflucasmendes.com.br/auth/login",
+      {
+        method: "POST",
+        headers:{
+          "Content-Type": "aplication/json"
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
+      }
+    )
+
+    const data = await response.json()
+    if(response.status === 200){
+      localStorage.setItem("Token_acess", data.token)
+    }
+    console.log(data)
+    setLoading(false)
+
+  }
+
+  return (
+    <section className="flex-1 flex items-center justify-center py-20">
+      <Card className="max-w-md border-border w-md">
+        <CardHeader className="text-center">
+
+          <div className="w-full flex justify-center mb-4">
+            <Brand />
+          </div>
+
+          <CardTitle className="text-2xl font-bold text-foreground">
+            Bem-vindo de volta
+          </CardTitle>
+          <CardDescription className="text-muted-foreground">
+            Entre com seu e-mail institucional
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <form className="flex flex-col gap-4" onSubmit={HandleSubmit}>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="email" className="text-foreground">E-mail institucional</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="seu.nome@ifce.edu.br"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+                required
+                className="h-11 bg-background"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center justify-between">
+                <Label htmlFor="password" className="text-foreground">Senha</Label>
+                <a href="/recover" className="text-primary text-sm">Esqueceu a senha?</a>
+              </div>
+
+              <div className="relative">
+                <Input
+                  id="password"
+                  name="password"
+                  type={ showPass ? "text" : "password" }
+                  placeholder="Digite sua senha"
+                  value={password}
+                  onChange={(e) => setPassowrd(e.currentTarget.value)}
+                  required
+                  className="h-11 bg-background"
+                />
+
+                <button
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-primary"
+                  type="button"
+                  onClick={() => setShowPass(prev => !prev)}
+                >
+                  { showPass ? <EyeOffIcon className="size-4" /> : <EyeIcon className="size-4" /> }
+                </button>
+              </div>
+            </div>
+
+            <Button type="submit" className="mt-2 h-11" disabled = {isLoading}>{isLoading ? <span> Entrando... </span> : "Entrar"}</Button>
+          </form>
+        </CardContent>
+
+        <CardFooter className="border-t border-border">
+          <p className="text-sm text-muted-foreground text-center w-full">
+            Não tem conta? <a href="/register" className="text-primary">Criar conta</a>
+          </p>
+        </CardFooter>
+      </Card>
+    </section>
+  )
+}
+
+export default LoginPage
+*/
