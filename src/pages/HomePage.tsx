2a20079cdabeba5380a832c3d778bf07a7b3d023
Marilia da Silva Feitosa <marilia.feitosa.contato@gmail.com>
2026-03-17 18:13:33 -0300

    :sparkles: feat: adição dos componentes da pasta homepage e implementação da lógica de Login

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 import CtaSection from "@/components/homepage/cta-section"
-//import FaqSection from "@/components/homepage/faq-section"
-//import FeatureSection from "@/components/homepage/feature-section"
+import FaqSection from "@/components/homepage/faq-section"
+import FeatureSection from "@/components/homepage/feature-section"
 import HeroSection from "@/components/homepage/hero-section"
 
 function HomePage() {
   return (
     <>
       <HeroSection />
-      {/* <FeatureSection />
-      //<FaqSection />*/}
+      <FeatureSection />
+      <FaqSection />
       <CtaSection />
     </>
   )
